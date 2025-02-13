219a7e110b6caa8833d1a9cf0bb5d5afd1a480b9
Ishira Pahasara <131232752+ishira-web@users.noreply.github.com>
2025-02-13 12:55:29 +0530

    Create The SpringBoot Project

---
+++ src/main/java/com/cabservice/megacity/MegacityApplication.java
@@ -0,0 +1,13 @@
+package com.cabservice.megacity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MegacityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MegacityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cabservice/megacity/MegacityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cabservice.megacity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MegacityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
