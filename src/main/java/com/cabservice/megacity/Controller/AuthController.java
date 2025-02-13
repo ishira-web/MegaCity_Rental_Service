87288937bdcc6182299db9b6b093d68e113cf8b7
Ishira Pahasara <131232752+ishira-web@users.noreply.github.com>
2025-02-13 19:03:34 +0530

    Create Auth Login Controller and Login DTO

---
+++ src/main/java/com/cabservice/megacity/Controller/AuthController.java
@@ -0,0 +1,50 @@
+package com.cabservice.megacity.Controller;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.cabservice.megacity.Dto.AllLoginDto;
+import com.cabservice.megacity.Dto.LoginResponse;
+import com.cabservice.megacity.Security.JWT.JwtUtils;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+
+@RestController
+@CrossOrigin(origins = "*")
+public class AuthController {
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @Autowired
+    private JwtUtils jwtUtils;
+
+    @PostMapping("/auth/login") 
+    public LoginResponse login(@RequestBody AllLoginDto loginDto) {
+    Authentication authentication = authenticationManager.authenticate(
+        new UsernamePasswordAuthenticationToken(loginDto.getUserName(), loginDto.getPassword())
+    );
+
+    SecurityContextHolder.getContext().setAuthentication(authentication);
+
+    String jwtToken = jwtUtils.generateJwtToken(authentication);
+
+    UserDetails userDetails = (UserDetails) authentication.getPrincipal();
+    String role = userDetails.getAuthorities().stream()
+            .findFirst()
+            .map(GrantedAuthority::getAuthority)
+            .orElse("USER");
+
+    String userId = jwtUtils.getUserIdFromJwtToken(jwtToken);
+
+    return new LoginResponse(jwtToken, role, userId);
+}
+
+}
---
+++ src/main/java/com/cabservice/megacity/Dto/LoginResponse.java
@@ -0,0 +1,20 @@
+package com.cabservice.megacity.Dto;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class LoginResponse {
+    
+    private String token;
+    private String role;
+    private String userId;
+
+
+    public LoginResponse(String token, String role, String userId) {
+        this.token = token;
+        this.role = role;
+        this.userId = userId;
+
+    }
+}
